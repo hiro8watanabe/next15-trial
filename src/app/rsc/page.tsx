9122aada792a2d216df2cefcad47f62e4ed62a53
hiro8watanabe <onecast.onelost615@gmail.com>
2025-05-24 20:58:51 +0900

    Operation test

--- src/app/rsc/page.tsx
+++ src/app/rsc/page.tsx
@@ -1,8 +1,13 @@
+import ClientComponent from "@/components/ClientComponent";
+import Link from "next/link";
+
 export default function ServerComponent() {
   console.log("ServerComponent");
   return (
     <div>
       <p>ServerComponent</p>
+      <ClientComponent />
+      <Link href="/about">About</Link>
     </div>
   );
 }
