b75cd442604e04186f9fe60368d415097def54df
hiro8watanabe <onecast.onelost615@gmail.com>
2025-05-25 12:06:53 +0900

    Enable verbose test output

--- jest.config.ts
+++ jest.config.ts
@@ -9,6 +9,7 @@ module.exports = {
     '^.+\\.(ts|tsx)$': ['ts-jest', { tsconfig: { jsx: 'react-jsx' } }],
     '^.+\\.(js|jsx)$': ['babel-jest', { presets: ['next/babel'] }],
   },
+  verbose: true,
   // globals: { // ts-jest の設定は transform に移動したためコメントアウトまたは削除
   //   'ts-jest': {
   //     tsconfig: {
